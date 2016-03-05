0e799c51f2ed2cfc0ea3699faf38fa70d59e7dc8
Santiago Torres <torresariass@gmail.com>
2016-03-05 16:39:33 -0500

    ENH: Adds container image (dockerfile + django app)

--- django/remote_viewing/peekhole/remote_browser.py
+++ django/remote_viewing/peekhole/remote_browser.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 
-def get_img_tag(url, browser=webdriver.Firefox()):
+def get_img_tag(url, browser=webdriver.PhantomJS()):
     browser.get(url)
     png = browser.get_screenshot_as_base64()
     img = "<img src='data:image/png;base64,{}'>".format(png)
--- django/remote_viewing/peekhole/views.py
+++ django/remote_viewing/peekhole/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse 
+from peekhole.remote_browser import get_img_tag
+
 # Create your views here.
+def get(request):
+
+    url = request.GET['target']
+
+    return HttpResponse(get_img_tag(url))
