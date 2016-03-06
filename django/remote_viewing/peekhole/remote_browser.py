0f910d3ef9e67924561bdcb75927f14ea9faa806
Ryan Brunt <rjhbrunt@gmail.com>
2016-03-05 20:45:38 -0500

    fixed window size

--- django/remote_viewing/peekhole/remote_browser.py
+++ django/remote_viewing/peekhole/remote_browser.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 
 def get_img_tag(url, browser=webdriver.PhantomJS()):
     browser.get(url)
+    browser.maximize_window()
     png = browser.get_screenshot_as_base64()
     img = "<img src='data:image/png;base64,{}'>".format(png)
     return img
